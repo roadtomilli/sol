d31df7d79ff756dbaa25af52decdd7f0b55f83cb
Jeremy Cho <jeremy@envoy.com>
2021-08-30 23:32:51 -0700

    setup.sh: make setup script more resilient

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Check if Homebrew exists, install it if not.
 which -s brew
 if [[ $? != 0 ]] ; then
@@ -8,11 +10,21 @@ else
     brew update
 fi
 
-# Check if Go exists, install it if not.
-GOVERSION=$(go version)
-if [[ "$GOVERSION" != *"1.17"* ]] ; then
-    echo "Correct Golang version not installed, installing..."
+# Check if high enough Go version exists, install it if not.
+which_go=$(which -s go)]
+if [[ $which_go != 0 ]] ; then
+    current_go_version=`go version | { read _ _ v _; echo ${v#go}; }`
+    required_go_version="1.17"
+    if [ "$(printf '%s\n' "$required_go_version" "$current_go_version" | sort -V | head -n1)" = "$required_go_version" ]; then 
+        echo "Go already installed, skipping..."
+    else
+        brew install go@1.17
+        echo "Correct Go version not installed, installing..."
+    fi
+else
+    echo "Go not installed, installing..."
     brew install go@1.17
+fi
 else
     echo "Golang already installed, skipping..."
 fi
